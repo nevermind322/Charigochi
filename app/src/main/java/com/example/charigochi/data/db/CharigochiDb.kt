329210c864e0bd74023df2859dbe2469e85726e9
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2024-05-01 19:20:32 +0300

    fix DB

--- app/src/main/java/com/example/charigochi/data/db/CharigochiDb.kt
+++ app/src/main/java/com/example/charigochi/data/db/CharigochiDb.kt
@@ -1,10 +1,11 @@
 package com.example.charigochi.data.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [CatEntity::class], version = 1)
 @TypeConverters(Converters::class)
-abstract class CharigochiDb {
+abstract class CharigochiDb : RoomDatabase(){
     abstract fun catDAO() : CatDAO
 }
