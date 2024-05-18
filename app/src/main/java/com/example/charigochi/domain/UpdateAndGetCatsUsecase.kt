89309b9a97dc81f5aa83f2bcc8a54f80ed3b69fb
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2024-05-18 16:10:50 +0300

    add cat death feature(

--- app/src/main/java/com/example/charigochi/domain/UpdateAndGetCatsUsecase.kt
+++ app/src/main/java/com/example/charigochi/domain/UpdateAndGetCatsUsecase.kt
@@ -19,8 +19,7 @@ class UpdateAndGetCatsUsecase @Inject constructor(private val catRepo: CatRepo) 
             for (cat in CATS_INIT) {
                 newCats.add(
                     if (!cat.unlocked) cat else cat.copy(
-                        lastBellyfulUpdate = date,
-                        happinessLastUpdate = date
+                        lastBellyfulUpdate = date, happinessLastUpdate = date
                     )
                 )
             }
@@ -28,10 +27,20 @@ class UpdateAndGetCatsUsecase @Inject constructor(private val catRepo: CatRepo) 
             if (cat.unlocked) {
                 val ms = date.time - cat.lastBellyfulUpdate!!.time
                 val hours = TimeUnit.HOURS.convert(ms, TimeUnit.MILLISECONDS).toInt()
-                val bellyful = (cat.bellyful - 3 * hours).let { if (it >= 0) it else 0 }
-                newCats.add(cat.copy(bellyful = bellyful, lastBellyfulUpdate = date))
-            } else
-                newCats.add(cat)
+                val bellyful = (cat.bellyful - 3 * hours).coerceIn(0..100)
+                val dead = bellyful <= 0
+                val happiness = (cat.happiness - 5 * hours).coerceIn(0..100)
+                newCats.add(
+                    cat.copy(
+                        bellyful = bellyful,
+                        lastBellyfulUpdate = date,
+                        happiness = happiness,
+                        happinessLastUpdate = date,
+                        unlocked = !dead,
+                        deaths = cat.deaths + if (dead) 1 else 0
+                    )
+                )
+            } else newCats.add(cat)
         }
 
         catRepo.upsertCat(*newCats.toTypedArray())
